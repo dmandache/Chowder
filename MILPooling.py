a895cd753ba6cfc1db0a83ba37a85ec4aa881046
dmandache <diana.mandache00@gmail.com>
2021-12-18 16:53:44 +0100

    first commit

---
+++ MILPooling.py
@@ -0,0 +1,36 @@
+from keras.layers import Layer, InputSpec, Flatten
+import tensorflow as tf
+import numpy as np
+
+class MILPooling(Layer):
+    """
+    MIL pooling layer that extracts the K-highest and K-lowest activations from a sequence (2nd dimension).
+    """
+    def __init__(self, kmax=1, kmin=0, **kwargs):
+        super().__init__(**kwargs)
+        self.input_spec = InputSpec(ndim=3)
+        self.kmax = kmax
+        self.kmin = kmin
+
+    def compute_output_shape(self, input_shape):
+        #return (input_shape[0], (input_shape[2] * self.k))
+        return (input_shape[0], self.kmax + self.kmin, input_shape[2])
+
+    def get_config(self):
+        config = {'kmin': self.kmin, 'kmax': self.kmax}
+        base_config = super(MILPooling, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def call(self, inputs):
+
+        # swap last two dimensions since top_k will be applied along the last dimension
+        shifted_input = tf.transpose(inputs, [0, 2, 1])
+
+        # extract top_k, returns two tensors [values, indices]
+        top_k = tf.nn.top_k(shifted_input, k=self.kmax, sorted=True, name=None)[0]
+        if self.kmin > 0:
+            bottom_k = tf.negative(tf.nn.top_k(tf.negative(shifted_input), k=self.kmin, sorted=True, name=None)[0])
+
+        out = tf.concat([top_k, bottom_k], -1) if self.kmin > 0 else top_k
+
+        return out
